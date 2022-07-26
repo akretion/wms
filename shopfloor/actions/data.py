aea906d603cf66f6e1bf61307f40be7297df6a42
Benoit <benoit.guillot@akretion.com>
2022-07-26 17:32:33 +0200

    improve french translation

--- shopfloor/actions/data.py
+++ shopfloor/actions/data.py
@@ -400,5 +400,6 @@ class DataAction(Component):
             "name",
             "date",
             "location_count",
+            "remaining_location_count",
             "inventory_line_count",
         ]
