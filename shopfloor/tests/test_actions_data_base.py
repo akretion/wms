ac213fbe5154fdafd747b2df13432cdb4759098c
Benoit <benoit.guillot@akretion.com>
2022-10-07 16:11:14 +0200

    remove missing tests

--- shopfloor/tests/test_actions_data_base.py
+++ shopfloor/tests/test_actions_data_base.py
@@ -106,16 +106,6 @@ class ActionsDataCaseBase(CommonCase, ActionsDataTestMixin):
         )
         cls.product_a_variant.flush()
         cls.product_a_vendor.flush()
-        cls.inventory = (
-            cls.env["stock.inventory"]
-            .sudo()
-            .create(
-                {
-                    "location_ids": [(6, 0, cls.stock_location.ids)],
-                    "prefill_counted_quantity": "zero",
-                }
-            )
-        )
 
     def _expected_location(self, record, **kw):
         data = {
@@ -188,16 +178,6 @@ class ActionsDataCaseBase(CommonCase, ActionsDataTestMixin):
             "total_quantity": sum(record.quant_ids.mapped("quantity")),
         }
         data.update(kw)
-        return data
-
-    def _expected_inventory(self, record, **kw):
-        data = {
-            "id": record.id,
-            "name": record.name,
-            "location_count": record.location_count,
-            "remaining_location_count": record.remaining_location_count,
-            "inventory_line_count": record.inventory_line_count,
-        }
         return data
 
 
